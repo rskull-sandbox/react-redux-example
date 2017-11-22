6bc1d1824ab103e9fddf6f3cb98b3c94da2d4b0b
rskull <info@rskull.com>
2017-11-23 00:16:15 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Reduxの処理の流れを出来るだけ１ファイルにまとめてみた
+
+元エントリー
+http://rskull.hateblo.jp/entry/2017/11/22/013039
+
+## Getting Started
+
+### 1.Install
+```
+$ git clone https://github.com/rskull/react-redux-example
+$ cd react-redux-example
+$ yarn
+```
+
+### 2.Run
+
+```
+$ yarn start
+```
---
+++ src/App.js
@@ -0,0 +1,29 @@
+// @flow
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux';
+import * as Actions from './index'
+
+class App extends Component<{}> {
+  render() {
+    const { todos, actions } = this.props
+    return (
+      <div>
+        <button onClick={() => actions.addItem('world')}>ADD</button>
+        <ul>
+          {todos.map((v, k) => <li onClick={() => actions.deleteItem(k)}>{v.item}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos
+})
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(Actions, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
